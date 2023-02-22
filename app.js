6486acbf938f148c098ab749925dc71dbc349fc6
benhurimmanuel <benhurimmanuel@gmail.com>
2023-02-22 09:40:14 +0530

    added some special character removal to my applycontractions

--- app.js
+++
@@ -1,6 +0,0 @@
-const Contractions = require("./index")
-
-
-const word = new Contractions("i'm benhur")
-
-console.log(word.applyContraction())
--- index.js
+++ index.js
@@ -1,63 +1,9 @@
+const contractionsList = require("./contractionsList")
 
-const contractionsList = new Map([
-    ["aren't", ["are not"]],
-    ["there's", ["there is", "there has"]],
-    ["can't", ["can not"]],
-    ["they'd", ["they had", "they would"]],
-    ["couldn't", ["could not"]],
-    ["they'll", ["they will", "they shall"]],
-    ["didn't", ["did not"]],
-    ["they're", ["they are"]],
-    ["doesn't", ["does not"]],
-    ["they've", ["they have"]],
-    ["don't", ["do not"]],
-    ["we'd", ["we had", "we would"]],
-    ["hadn't", ["had not"]],
-    ["we're", ["we are"]],
-    ["hasn't", ["has not"]],
-    ["we've", ["we have"]],
-    ["haven't", ["have not"]],
-    ["weren't", ["were not"]],
-    ["he'd", ["he had", "he would"]],
-    ["what'll", ["what will", "what shall"]],
-    ["he'll", ["he will", "he shall"]],
-    ["what're", ["what are"]],
-    ["he's", ["he is", "he has"]],
-    ["what's", ["what is", "what has"]],
-    ["i'd", ["I had", "I would"]],
-    ["what've", ["what have"]],
-    ["i'll", ["I will", "I shall"]],
-    ["where's", ["where is", "where has"]],
-    ["i'm", ["I am"]],
-    ["who'd", ["who had", "who would"]],
-    ["i've", ["I have"]],
-    ["who'll", ["who will", "who shall"]],
-    ["isn't", ["is not"]],
-    ["who're", ["who are"]],
-    ["let's", ["let us"]],
-    ["who's", ["who is, who has"]],
-    ["mightn't", ["might not"]],
-    ["who've", ["who have"]],
-    ["mustn't", ["must not"]],
-    ["won't", ["will not"]],
-    ["shan't", ["shall not",]],
-    ["wouldn't", ["would not"]],
-    ["she'd", ["she had", "she would"]],
-    ["you'd", ["you had", "you would"]],
-    ["she'll", ["she will", "she shall"]],
-    ["you'll", ["you will", "you shall"]],
-    ["she's", ["she is", "she has"]],
-    ["you're", ["you are"]],
-    ["shouldn't", ["should not"]],
-    ["you've", ["you have"]],
-    ["that's", ["that is", "that has"]],
-    ["y'all", ["you all"]]
-]
-)
 
 class Contractions {
-    constructor(sentence) {
-        this.wordArray = sentence.toLowerCase().split(" ")
+    constructor() {
+        this.specialCharacters = [",", "/", "_", "-"]
     }
     addContraction = (word, value) => {
         if (contractionsList.get(word)) {
@@ -76,15 +22,29 @@ class Contractions {
         if (contractionsList.get(word)) return 1
         else return 0
     }
-    applyContraction = () => {
+    applyContraction = (sentence) => {
+        this.wordArray = sentence.toLowerCase().split(" ")
+        let cleanElement
+
         this.wordArray.forEach((element, index) => {
-            const contractionWord = this.getContraction(element)
+            for (let i in this.specialCharacters) {
+                if (element.includes(this.specialCharacters[i])) {
+                    cleanElement = element.replace(",", "")
+                    break;
+                }
+                else {
+                    cleanElement = element
+                }
+            }
+            const contractionWord = this.getContraction(cleanElement)
             if (contractionWord) {
                 this.wordArray[index] = contractionWord
             }
         });
         return (this.wordArray.join(" "))
     }
-
 }
-module.exports = Contractions
+
+
+const contraction = new Contractions()
+module.exports = contraction
